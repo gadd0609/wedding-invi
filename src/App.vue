24bda857b38865e42cadc69002e3b9efd0be104b
gadd0609 <alegaddiel@gmail.com>
2025-02-23 03:18:11 -0600

    Feat(Configs):Test

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,6 @@
   <fiances-photos/>
   <gift-table-component/>
   <form-component/>
-  <form name="contact" netlify>
-  <p>
-    <label>Name <input type="text" name="name" /></label>
-  </p>
-  <p>
-    <label>Email <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
 </template>
 
 <script>

b80c25231cb39e7a88efe4fa0c14c09cb55e6615
gadd0609 <alegaddiel@gmail.com>
2025-02-23 03:13:34 -0600

    Feat(Configs):Test

--- src/components/form/index.vue
+++ src/components/form/index.vue
@@ -1,4 +1,15 @@
 <template>
+    <form name="contact" netlify>
+  <p>
+    <label>Name <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Email <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
     <form name="contact" method="POST" netlify>
         <input type="hidden" name="form-name" value="contact" />
       <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">

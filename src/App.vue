72a4585e929b25158c57fbf9a7a4d7c20a0c1685
gadd0609 <alegaddiel@gmail.com>
2025-06-06 01:13:55 -0600

    Feat(): Welcome and video

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@
 
 <script>
 import WelcomeComponent from './components/welcome/index.vue'
-import VideoComponent from './components/videoComponent/index.vue'
+import VideoComponent from './components/VideoComponent/index.vue'
 import FiancesPhotos from './components/fiances/index.vue'
 import GiftTableComponent from './components/giftTable/index.vue'
 import FormComponent from './components/form/index.vue'

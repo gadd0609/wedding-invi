08852fd5c52ab26168df370f1f091fec95a33921
gaddiel.patino <gaddiel.patino@cimagroup.com.mx>
2025-08-13 12:30:09 -0600

    Fix(): Video component

--- src/components/VideoComponent/index.vue
+++ src/components/VideoComponent/index.vue
@@ -3,7 +3,7 @@
     <div class="relative mx-auto max-w-4xl flex flex-col items-center">
       <video controls width="640">
         <source 
-          src="https://drive.google.com/drive/shared-with-me?hl=es" 
+          src="https://drive.google.com/file/d/1x5-ehPMVmwBMxzAuRaXiG-Y4p9P1--pb/view?usp=sharing" 
           type="video/mp4">
       </video>
     </div>

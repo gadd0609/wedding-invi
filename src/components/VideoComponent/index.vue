20809b4e012fbd05acd68e111248c37c454619c8
gaddiel.patino <gaddiel.patino@cimagroup.com.mx>
2025-08-13 18:22:37 -0600

    Fix(): Video component

--- src/components/VideoComponent/index.vue
+++ src/components/VideoComponent/index.vue
@@ -1,17 +1,15 @@
 <template>
   <div class="bg-blue-custom py-24 sm:py-32">
     <div class="relative mx-auto max-w-4xl flex flex-col items-center">
-      <iframe 
-        width="560" 
-        height="315" 
-        src="https://www.youtube.com/embed/UQKh7SjgAG8?si=m0SKm2W5pJbBSeTB&amp;start=1" 
-        title="YouTube video player" 
-        frameborder="0" 
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-        referrerpolicy="strict-origin-when-cross-origin" 
+      <iframe
+        class="w-72 h-[500px] sm:w-96 sm:h-[600px] rounded-xl shadow-lg"
+        src="https://www.youtube.com/embed/UQKh7SjgAG8?si=m0SKm2W5pJbBSeTB&amp;start=1"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
         allowfullscreen
-      >
-      </iframe>
+      ></iframe>
     </div>
   </div>
 </template>

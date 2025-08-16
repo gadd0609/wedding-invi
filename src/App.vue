55dc6ae564bc959ccf6f2c51d2c018a1b080dcca
gaddiel.patino <gaddiel.patino@cimagroup.com.mx>
2025-08-16 13:22:21 -0600

    Fix(): Ajuste en componentes

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,25 @@
 <template>
-  <div>
-    <welcome-component/>
-    <SectionFade class="bg-beige">
-      <video-component/>
-    </SectionFade>
-    <SectionFade class="bg-blue-custom">
-      <countdown-component/>
-      <fiances-photos/>
-    </SectionFade>
-    <SectionFade class="bg-orange-custom">
-      <info-component/>
-    </SectionFade>
-    <SectionFade>
-      <gift-table-component/>
-    </SectionFade>
-    <SectionFade>
-      <form-component
-        name="rsvp"
-        method="post"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-      />
-    </SectionFade>
+  <welcome-component/>
+  <div class="bg-beige">
+    <video-component/>
   </div>
+  <div class="bg-blue-custom">
+    <countdown-component/>
+    <fiances-photos/>
+  </div>
+  <div class="bg-orange-custom">
+    <info-component/>
+  </div>
+  <gift-table-component/>
+  <form-component
+    name="rsvp"
+    method="post"
+    data-netlify="true"
+    data-netlify-honeypot="bot-field"
+  />
 </template>
+
 <script>
-import { ref, onMounted, defineComponent, h } from 'vue'
 import WelcomeComponent from './components/welcome/index.vue'
 import VideoComponent from './components/VideoComponent/index.vue'
 import FiancesPhotos from './components/fiances/index.vue'
@@ -33,41 +27,6 @@ import InfoComponent from './components/info/index.vue'
 import GiftTableComponent from './components/giftTable/index.vue'
 import FormComponent from './components/form/index.vue'
 import CountdownComponent from './components/countdown/index.vue'
-
-const SectionFade = defineComponent({
-  name: 'SectionFade',
-  props: {
-    class: { type: String, default: '' }
-  },
-  setup(props, { slots }) {
-    const el = ref(null)
-    const visible = ref(false)
-    onMounted(() => {
-      const observer = new window.IntersectionObserver(
-        ([entry]) => {
-          if (entry.isIntersecting) visible.value = true
-        },
-        { threshold: 0.15 }
-      )
-      if (el.value) observer.observe(el.value)
-    })
-    return () =>
-      h(
-        'div',
-        {
-          ref: el,
-          class: [
-            'min-h-screen w-full flex flex-col transition-all duration-700 ease-out',
-            visible.value
-              ? 'opacity-100 translate-y-0'
-              : 'opacity-0 translate-y-12',
-            props.class
-          ]
-        },
-        slots.default && slots.default()
-      )
-  }
-})
 
 export default {
   name: 'IndexPage',
@@ -78,10 +37,8 @@ export default {
     InfoComponent,
     GiftTableComponent,
     FormComponent,
-    CountdownComponent,
-    SectionFade
+    CountdownComponent
   }
 }
 </script>
-
 
